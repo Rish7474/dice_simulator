f7059dbce6e6a7ee8926379e2b4ebf3d2794925c
Rishabh Mehta <44340780+Rish7474@users.noreply.github.com>
2019-06-20 16:20:13 -0500

    Update
    Added a while loop which allows the program to run for a desired time
    Added average rolled value

--- dice_simulator.py
+++ dice_simulator.py
@@ -2,13 +2,21 @@
 from math import floor
 from random import randint
 dice_vals = []
-dice_num = int(input("How many die are you rolling? "))
-expected_snake_eyes = dice_num/36 #1/36 is the probabilty of getting 1 snake eyes
-print("Expected number of snake eyes: ",expected_snake_eyes)
-snake_eyes = 0
-for x in range(dice_num):
-    dice_vals.append(randint(1,6))
-    if x > 0 and dice_vals[x-1] + dice_vals[x] == 2:
-        snake_eyes+=1
-print("You rolled the following value(s): ",dice_vals)
-print("Number of snake eyes: ",snake_eyes)
+yes = True
+while(yes):
+    sum_of_rolls = 0
+    snake_eyes = 0
+    trials = int(input("How many die are you rolling? "))
+    expected_snake_eyes = trials/36 #1/36 is the probabilty of getting 1 snake eyes
+    print("Expected number of snake eyes: ",expected_snake_eyes)
+    for x in range(trials):
+        rolled_val = randint(1,6)
+        sum_of_rolls+=rolled_val
+        dice_vals.append(rolled_val)
+        if x > 0 and dice_vals[x-1] + dice_vals[x] == 2:
+            snake_eyes+=1
+    print("You rolled the following value(s): ",dice_vals)
+    print("Number of snake eyes: ",snake_eyes)
+    print("Average rolled value: ",sum_of_rolls/trials,"\n") #Equals 3.5 as trials increases
+    if input("Would you like to go again? y/n: ") == "n":
+        yes = False
