5b6f4d6a2872d54f1fe7512a6ecce9f01224b434
Rishabh Mehta <44340780+Rish7474@users.noreply.github.com>
2019-06-19 19:54:27 -0500

    Expected amount of snake eyes
    Calculates the expected amount of snake eyes for given number of trials. This can be used to compare the actual results.

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,11 +1,14 @@
 #Dice Simulator Program
+from math import floor
 from random import randint
 dice_vals = []
 dice_num = int(input("How many die are you rolling? "))
+expected_snake_eyes = dice_num/36
+print("Expected number of snake eyes: ",expected_snake_eyes)
 snake_eyes = 0
 for x in range(dice_num):
     dice_vals.append(randint(1,6))
-    print("You rolled a",dice_vals[x])
+    #print("You rolled a",dice_vals[x])
     if x > 0 and dice_vals[x-1] + dice_vals[x] == 2:
         snake_eyes+=1
-print(snake_eyes)
+print("Number of snake eyes: ",snake_eyes)
